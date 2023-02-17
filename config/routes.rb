549f21661e75a5b9044387b4b01aa8a93fa4205a
radheshyamrathore <rsrathore002@gmail.com>
2023-02-17 16:39:41 +0530

    add devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
 
-  root "comments#index"
+  devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
+  root "users#index"
+  resources :users
   resources :articles do 
     resources :comments
   end
