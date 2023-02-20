d0e6fafe8bf8ac532ce293067939962a999bcbfc
radheshyamrathore <rsrathore002@gmail.com>
2023-02-20 14:51:14 +0530

    proper intent controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
-  before_action :set_article, on: [:update, :destroy, :create]
+  before_action :set_article, only: [:show, :update, :destroy, :create]
 
   def index
-    @comments =  @article.comments
+    @comments =  Comment.all
   end
 
   def show
@@ -19,12 +19,10 @@ class CommentsController < ApplicationController
 
   def create
     @comment = @article.comments.create(comment_params.merge(user_id: current_user.id))
-      redirect_to @article
+    redirect_to @article
   end
 
   def destroy
-    # @article = Article.find(params[:article_id])
-    #@comment = @article.comments.find(comment_params[:comment_id])
     @comment = Comment.find(params[:id])
     @comment.article_id = params[:article_id]
     if @comment.destroy
@@ -49,7 +47,7 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
   end
 
-    def comment_params
-      params.require(:comment).permit(:commenter, :description, :article_id, :user_id)
-    end
+  def comment_params
+    params.require(:comment).permit(:commenter, :description, :article_id, :user_id)
+  end
 end
