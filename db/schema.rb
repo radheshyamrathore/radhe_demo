9471cbc2732067c7699ccf3f1749621a2fd9d995
radheshyamrathore <rsrathore002@gmail.com>
2023-02-24 18:28:31 +0530

    admin editor or user funtionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_24_055348) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_123732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_055348) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
     t.string "description"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
