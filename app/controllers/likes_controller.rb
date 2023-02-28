9e43c4cbab102446839a30bc7cc4b2664d5a675c
radheshyamrathore <rsrathore002@gmail.com>
2023-02-28 16:45:19 +0530

    added like and unlike button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,7 @@
 class LikesController < ApplicationController
-  before_action :set_article, only: [:create, :destroy]
-  before_action :find_post, only: [:destroy, :create]
+   before_action :set_article, only: [:create, :destroy]
+   before_action :find_post, only: [:destroy, :create]
+  # before_action :find_like, only: [:destroy]
 
   def index
     @likes = Like.all
@@ -10,19 +11,33 @@ class LikesController < ApplicationController
     @like =  Like.new
   end
 
+
   def create
-    @like = @post.likes.new
-    @like.user_id = current_user.id
-    if @like.save 
-      redirect_to article_post_path(@article, @post)
+    if already_liked?
+      flash[:notice] = "You can't like more than once"
     else
-      render :new
+      @post.likes.create(user_id: current_user.id)
     end
+    redirect_to article_post_path(@article, @post)
+  end
+
+  def destroy
+    @like = @post.likes.find(params[:id])
+    if !(already_liked?)
+      flash[:notice] = "Cannot unlike"
+    else
+      @like.destroy
+    end
+    redirect_to article_post_path(@article, @post)
   end
 
   def already_liked?
     Like.where(user_id: current_user.id, post_id:
     params[:post_id]).exists?
+  end
+
+  def find_like
+   @like = @post.likes.find(params[:id])
   end
 
   private
