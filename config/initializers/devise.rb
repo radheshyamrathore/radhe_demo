04f4b225a5c8813f7aeee55075568f068c2a1e53
radheshyamrathore <rsrathore002@gmail.com>
2023-03-07 17:41:14 +0530

    add smtp for email confirm To user

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,7 +25,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'rsrathore002@gmail.com'
-
+  config.mailer = 'UserMailer'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
